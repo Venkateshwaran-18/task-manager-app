7e02d87063d5c407e83b97ea782b3a19da2d201e
Venkateshwaran-18 <venkat18062000@gmail.com>
2026-04-05 17:09:56 +0530

    Uploading files as a structured folder

---
+++ TaskManager/TaskManager.Domain/Entities/TaskItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Domain.Entities
+{
+    public class TaskItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public Guid? UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
