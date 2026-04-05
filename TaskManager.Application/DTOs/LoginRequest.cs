51b2099e46c9b797e8a2e64c955518df5a150875
Venkateshwaran-18 <venkat18062000@gmail.com>
2026-04-05 17:10:54 +0530

    Delete TaskManager.Application directory

--- TaskManager.Application/DTOs/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TaskManager.Application.DTOs
-{
-    public class LoginRequest
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
--- TaskManager.Application/DTOs/RegisterRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace TaskManager.Application.DTOs
-{
-    public class RegisterRequest
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
